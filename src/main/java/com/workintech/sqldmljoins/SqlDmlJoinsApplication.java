d014d91232647002131d9b4c5749d6e6f1ff8cb4
onur emre <149792471+xthehonour@users.noreply.github.com>
2026-05-29 23:26:21 +0300

    proje bitimi

--- src/main/java/com/workintech/sqldmljoins/SqlDmlJoinsApplication.java
+++ src/main/java/com/workintech/sqldmljoins/SqlDmlJoinsApplication.java
@@ -3,8 +3,14 @@ package com.workintech.sqldmljoins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class SqlDmlJoinsApplication {
+
+	static {
+		Locale.setDefault(Locale.US);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SqlDmlJoinsApplication.class, args);
